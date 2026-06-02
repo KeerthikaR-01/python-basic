2efb4f4b90db85e75e7b729879b301a8e8586de5
KeerthikaR-01 <keerthika21201@gmail.com>
2026-06-02 13:08:34 +0530

    implement variables, output and input

---
+++ intro.py
@@ -0,0 +1,21 @@
+# variables
+
+name = "John"
+age = 30    
+city = "New York"
+
+# Output
+print(name)
+print(age)
+print(city)
+
+print(name, age, city)
+
+#input
+
+name1 = input("Enter your name: ")
+print(name1)
+
+print('Name:', name1)
+print('Age:', age)
+print('City:', city)
