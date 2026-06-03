d56815779f4cb558c2e868055983288365cc3290
KeerthikaR-01 <keerthika21201@gmail.com>
2026-06-03 11:23:47 +0530

    implementaion of strings

---
+++ Day2.py
@@ -0,0 +1,31 @@
+# Strings
+
+city = "Bangalore"
+print(city)
+
+# Joining Strings
+
+first_name = "Alice"
+last_name = "Smith"
+
+full_name = first_name + " " + last_name
+print(full_name)
+
+# FInding Length of a String
+
+# name = "Alice"
+length = len(full_name)
+print(length)
+
+# Converting to Uppercase and Lowercase
+
+fruit1 = "apple"
+fruit2 = "CHERRY"
+upper_name = fruit1.upper()
+lower_name = fruit2.lower()
+
+print(upper_name)
+print(lower_name)
+
+name = input("Enter your name: ")
+print(name.upper())
